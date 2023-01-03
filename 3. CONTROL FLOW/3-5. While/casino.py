74305cb8765536681fad0f3e807f7cdc4d10ae1c
yubeenso <youbin0105@gmail.com>
2023-01-03 23:51:37 +0900

    chapter 3 commit 2

---
+++ 3. CONTROL FLOW/3-5. While/casino.py
@@ -0,0 +1,16 @@
+# from random import randint
+
+# user_choice = int(input("Choose number."))
+# pc_choice = randint(1, 50)
+
+# if user_choice == pc_choice:
+#   print("You won!")
+# elif user_choice > pc_choice:
+#   print("Lower! Computer chose", pc_choice)
+# elif user_choice < pc_choice:
+#   print("Higher! Computer chose", pc_choice)
+
+distance = 0
+
+while distance < 20:
+  print("I'm running:", distance, "km")
