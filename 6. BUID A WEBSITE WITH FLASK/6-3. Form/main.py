7b80f784a9ad3205b3be349a32aff14f36626895
yubeenso <youbin0105@gmail.com>
2023-01-13 23:45:07 +0900

    chapter 6 commit 1

---
+++ 6. BUID A WEBSITE WITH FLASK/6-3. Form/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask("Jobscrapper")
+
+
+@app.route("/")
+def home():
+  return render_template("home.html", name="nico")
+
+
+@app.route("/search")
+def hello():
+  return render_template("search.html")
+
+
+app.run("0.0.0.0")
---
+++ 6. BUID A WEBSITE WITH FLASK/6-6 For Loops/main.py
@@ -0,0 +1,22 @@
+rom flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+
+app = Flask("Jobscrapper")
+
+
+@app.route("/")
+def home():
+  return render_template("home.html", name="nico")
+
+
+@app.route("/search")
+def search():
+  keyword = request.args.get("keyword")
+  indeed = extract_indeed_jobs(keyword)
+  wwr = extract_wwr_jobs(keyword)
+  jobs = indeed + wwr
+  return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+app.run("0.0.0.0")
